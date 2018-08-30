119b2f7fe6c0ed06414bd1b82c3fd325f61162de
xsnakee <xsnakee@gmail.com>
2018-08-30 23:40:23 +0300

    added User interface type

---
+++ SL_TestProj_TaskManager/UserInterface_t.cs
@@ -0,0 +1,31 @@
+﻿namespace SL_TestProj_TaskManager
+{
+    public class UserInterface_t
+    {
+        //FIELDS
+
+        //METH
+        public UserInterface_t() { }
+
+        private bool AddTask() {
+            return false;
+        }
+
+        private bool DeleteTask()
+        {
+            return false;
+        }
+        private void ShowList()
+        {
+        }
+
+        private Task_t SearchTask(uint ID)
+        {
+            return new Task_t("Test content");
+        }
+
+        private void PrintMenu() { }
+        private void KeyEventHandler() { }
+        public void Show() { }
+    }
+}
