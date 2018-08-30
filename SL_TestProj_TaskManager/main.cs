3a5117960b64b0ec3ed562a079292564df43e8f1
xsnakee <xsnakee@gmail.com>
2018-08-30 22:59:10 +0300

    init proj, updated gitignore

---
+++ SL_TestProj_TaskManager/main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SL_TestProj_TaskManager
+{
+    public static class main
+    {
+        public static void Main()
+        {
+            Console.WriteLine("INIT");
+            Console.ReadKey();
+            return;
+        }
+    }
+}
