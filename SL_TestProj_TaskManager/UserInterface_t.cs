1e09b9de6da6a8b434fca324f69c7b18e3735d81
xsnakee <xsnakee@gmail.com>
2018-08-31 11:47:36 +0300

    Revert "added TaskList type, moved searchTask func to  TaskList type"
    This reverts commit 0f4aa7e07d4d7c8c6488557384b03a744adc0b08.

--- SL_TestProj_TaskManager/UserInterface_t.cs
+++ SL_TestProj_TaskManager/UserInterface_t.cs
@@ -18,7 +18,12 @@
         private void ShowList()
         {
         }
-        
+
+        private Task_t SearchTask(uint ID)
+        {
+            return new Task_t("Test content");
+        }
+
         private void PrintMenu() { }
         private void KeyEventHandler() { }
         public void Show() { }
