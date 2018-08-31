4a7c7934ec75b2ac33741dc2bf83c1e517ce7d07
xsnakee <xsnakee@gmail.com>
2018-08-31 13:49:40 +0300

    restored TaskList_t, updated add, edit ,delete, search and togglestatus methods

---
+++ SL_TestProj_TaskManager/TaskList_t.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace SL_TestProj_TaskManager
+{
+    public class TaskList_t
+    {
+        //PROP
+        List<Task_t> TaskList;
+        //METH
+        public TaskList_t() {
+            TaskList = new List<Task_t>();
+        }
+
+        public Task_t GetTask(uint ID)
+        {
+            return SearchTask(ID);
+        }
+
+        public void AddTask(string Content)
+        {
+            TaskList.Add(new Task_t(Content));
+        }
+
+        public bool EditTask(uint ID, string NewContent)
+        {
+            Task_t tempTask = SearchTask(ID);
+            if (tempTask != null)
+            {
+                tempTask.ChangeContent(NewContent);
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeleteTask(uint ID)
+        {
+            Task_t tempTask = SearchTask(ID);
+            if (tempTask != null)
+            {
+                TaskList.Remove(tempTask);
+                return true;
+            }
+            return false;
+        }
+
+        public bool ToggleTaskStatus(uint ID)
+        {
+            Task_t tempTask = SearchTask(ID);
+            if (tempTask != null)
+            {
+                return tempTask.ToggleStatus();
+            }
+            return false;
+        }
+
+        private Task_t SearchTask(uint ID)
+        {
+            foreach(Task_t tempTask in TaskList)
+            {
+                if (tempTask.Id == ID)
+                {
+                    return tempTask;
+                }
+            }
+            return null;
+        }
+    }
+}
